0bdb273e27f1babc9553dac70c74643039f72cdb
Chelcea Claudiu <claudiu.chelcea@stud.acs.upb.ro>
2020-12-29 21:44:06 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #include <string.h>
 #include "Support.h"
 #include "Memory.h"
-#define particular_case "SELECT"
-#define MAX_COMMAND_SIZE 10
 #define DEFINED_COMMANDS 9
 
 int main(void) {
@@ -18,10 +16,15 @@ int main(void) {
     char *input = user_input();
     
     // Receive commands and execute them
-    scanf("%s",input);
-    int command_value = find_command_value(commands, input);
-    check_command(command_value);
+    while(1) {
+        scanf("%s",input);
 
+        // Check what command is given and if it exists.
+        int command_value = find_command_value(commands, input);
+
+        // If it exists, execute it.
+        check_command(command_value);
+    }
 
     free(input);
 
