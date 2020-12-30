88347fde81d53339d4f3751e5df766dc03e2db5b
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2020-12-30 23:25:24 +0200

    Update Memory.h

--- Photo Editor/Memory.h
+++ Photo Editor/Memory.h
@@ -1,10 +1,15 @@
-
 // Chelcea Claudiu Marian
 
 #ifndef __MEMORY_H_
 #define __MEMORY_H_
+#include <stdio.h>
 
-// This function allocates the space necessary for a command.
+// This function allocates the space necessary for the commands.
 char* user_input(void);
+
+// This function returns a value between 0 - 7 representing
+// the position in the command list of the received input
+// and returns -1 if it doesn't exist
+int find_command_value(const char**, const char*);
 
 #endif
