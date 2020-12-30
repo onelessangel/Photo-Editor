85d887e857559ac67db39e74b5e8c3abf1951fa4
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2020-12-30 23:25:02 +0200

    Update Memory.c

--- Photo Editor/Memory.c
+++ Photo Editor/Memory.c
@@ -1,7 +1,10 @@
 // Chelcea Claudiu Marian
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_COMMAND_SIZE 10
+#define DEFINED_COMMANDS 9
 
 // Exit status
 enum {
@@ -17,4 +20,22 @@ char* user_input(void) {
         return (char* ) MALLOC_FAIL;
     
     return input;
+}
+
+// This function returns a value between 0 - 8 representing
+// the position in the command list of the received command
+// and returns -1 if it doesn't exist
+int find_command_value(const char *commands[], char* input)
+{
+    int command_value=-1;
+    for(int i=0;i<DEFINED_COMMANDS;i++) 
+        if(strcmp(commands[i],input)==0) {
+            command_value=i;
+            break;
+        }
+
+    if(strcmp("ALL",input)==0 || command_value == -1) 
+        command_value=2;
+
+    return command_value;
 }
