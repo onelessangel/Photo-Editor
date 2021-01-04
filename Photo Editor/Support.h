4ed02ac131af6e4f911aaed52a8aa406bd4f9601
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-04 23:15:05 +0200

    Update Support.h

--- Photo Editor/Support.h
+++ Photo Editor/Support.h
@@ -6,7 +6,7 @@
 
 // This function loads the image from the file
 // and keeps the width and the height.
-unsigned char** LOAD(const char*,int*,int*,int*,int*,int*,int*,int*,char*,int*);
+unsigned char** LOAD(const char*,int*,int*,int*,int*,int*,int*,int*,char*,int*,int*);
 
 // This function selects a part of the area.
 void SELECT(int*, int*, int*, int*, int*, int*);
@@ -23,10 +23,13 @@ void CROP(int*,int*,int*,int*,int*,int*);
 // This function will turn the image to grayscale format.
 void GRAYSCALE(int*,int*,int*,int*,unsigned char***);
 
+// This functions returns the lowest value from the comparison of two values
+int minimum_value(int, int);
+
 // This function will turn the image to sepia format.
 void SEPIA(int*,int*,int*,int*,unsigned char***,int*);
 
-// This function saves the current image to a file.
+// This function saves the current image to a file in the specified format.
 void SAVE(const char*, const char*,unsigned char**,const int,const int);
 
 // This function closes the program and frees the memory.
@@ -34,10 +37,8 @@ void EXIT(char**,unsigned char***,const int);
 
 // This functions checks if an valid command has been received
 // and, if true, executes the corresponding function.
-void check_command(const int, int*, int*,int*,int*, int*,int*,int*,unsigned char***,char**,int*,char*,int*);
+void check_command(const int, int*, int*,int*,int*, int*,int*,int*,unsigned char***,char**,int*,char*,int*,int*);
 
-// This functions returns the lowest value from the comparison of two values
-int minimum_value(int, int);
 
 // Exit status
 enum {
