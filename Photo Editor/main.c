f141da4434d8ac9ac09f87bbeacc3c5b399449c5
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-05 21:19:04 +0200

    Update main.c

--- Photo Editor/main.c
+++ Photo Editor/main.c
@@ -19,9 +19,9 @@ int main(void) {
     // Set IMAGE
     unsigned char **image = NULL;
     int width = 0, height = 0, image_status=DOWN;
-    int x1=0,y1=0,x2=0,y2=0;
-    int color_image=0, my_image_max=0, no_image_loaded = 1;
-    char type = 't';
+    int x1=0,y1=0,x2=0,y2=0, my_image_max=0;
+    int color_image=0, no_image_loaded = 1;
+    char type = '0';
 
     // Set COMMAND
     const char *commands[DEFINED_COMMANDS] = {"LOAD","SELECT","SELECT ALL","ROTATE","CROP","GRAYSCALE","SEPIA","SAVE","EXIT"};
