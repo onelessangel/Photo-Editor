b9e67e3baad1fd1f09a9867c271469fe9652a686
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-07 18:37:21 +0200

    Update Memory.c

--- Photo Editor/Memory.c
+++ Photo Editor/Memory.c
@@ -3,13 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX_COMMAND_SIZE 100
-#define DEFINED_COMMANDS 9
 
 // Exit status
 enum {
     MALLOC_FAIL=2,
     REALLOC_FAIL=3
+};
+
+// Commands sizes
+enum {
+    MAX_COMMAND_SIZE=100,
+    DEFINED_COMMANDS=9
 };
 
 // This function allocates the space necessary for a command.
@@ -27,38 +31,25 @@ char* user_input(void) {
 // and returns -1 if it doesn't exist
 int find_command_value(const char *commands[], char* input)
 {
-    // Check if command is in list
+    // Receive command
     int command_value=-1;
+    char* first_command = (char*) malloc(MAX_COMMAND_SIZE * sizeof(char));
+    if(first_command==NULL)
+        return  MALLOC_FAIL;
+    char* second_command = (char*) malloc(MAX_COMMAND_SIZE * sizeof(char));
+    if(second_command==NULL)
+        return  MALLOC_FAIL;
+    sscanf(input,"%s %s",first_command,second_command);
+
+    // Check if the command is in the list
     for(int i=0;i<DEFINED_COMMANDS;i++) 
-        if(strcmp(commands[i],input)==0) {
+        if(strcmp(commands[i],first_command)==0) {
             command_value=i;
             break;
         }
-    char a=0,b=0,c=0,d=0;
-    char* wrong_input = (char*) malloc(100*sizeof(char));
+    if(command_value == 1 && (strcmp(second_command,"ALL")==0))
+        command_value=2;
 
-    // Check for particular cases e.g.: SELECT ALL or extra input
-    if(command_value==1)  {
-        scanf("%c%c%c%c",&a,&b,&c,&d);
-        if(a!=' ' || b!='A' || c!='L' || d != 'L') {
-            ungetc(d,stdin);
-            ungetc(c,stdin);
-            ungetc(b,stdin);
-            ungetc(a,stdin);
-        
-        } else {
-            ungetc(d,stdin);
-            ungetc(c,stdin);
-            ungetc(b,stdin);
-            ungetc(a,stdin);
-            scanf("%s",wrong_input);
-            if(strlen(wrong_input) == 3)
-                command_value=2;
-            else command_value=-1;
-        }
-    }
-        
-    free(wrong_input);
     return command_value;
 }
 
@@ -87,7 +78,7 @@ unsigned char** read_pixels_binary(const int my_image_width,const int my_image_h
     unsigned char** pixel_matrix = (unsigned char**) malloc(my_image_width * sizeof(unsigned char*));
     for(int i=0;i<my_image_height;i++) 
         pixel_matrix[i] = (unsigned char*) malloc(my_image_height * sizeof(unsigned char));
-
+        
     // Scan values from BINARY file
     for(int i=0;i<my_image_height;i++) 
         for(int j=0;j<my_image_width;j++) 
