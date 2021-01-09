339adcbeb4e4091b44432238a6909f1f5613a105
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-09 16:33:47 +0200

    Update Program.c

--- Photo Editor/Program.c
+++ Photo Editor/Program.c
@@ -15,7 +15,7 @@ int main(void) {
 	unsigned char **image = NULL;
 	int width = 0, height = 0, image_status = DOWN;
 	int x1 = 0, y1 = 0, x2 = 0, y2 = 0, my_image_max = 0, cropped=0;
-	int print_x1=0,print_x2= 0,print_y1= 0,print_y2 = 0;
+	int print_x1=0,print_x2= 0,print_y1= 0,print_y2 = 0, cropped_now=0;
 	int color_image = 0, whole_map_selected = 1, correct = 0, has_been_cropped=0;
 	char type = '0';
 
@@ -37,7 +37,7 @@ int main(void) {
 		check_command(command_value, &width, &height, &image_status, &x1, &y1,
 				&x2, &y2, &image, &input, &color_image, &type, &my_image_max,
 				&whole_map_selected, &correct,&cropped,&has_been_cropped,
-				&print_x1,&print_y1,&print_x2,&print_y2);
+				&print_x1,&print_y1,&print_x2,&print_y2,&cropped_now);
 	}
 
 	// Free memory
