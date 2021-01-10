fb708b8a72a4e1286eab4309a56adfce2037259c
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-10 22:29:01 +0200

    Update Commands.h

--- Photo Editor/Commands.h
+++ Photo Editor/Commands.h
@@ -6,52 +6,55 @@
 
 // This function loads the image from the file
 // and keeps the width and the height.
-unsigned char **LOAD(const char *, int *, int *, int *, int *, int *, int *,
-int *, int *, char *);
+unsigned char **LOAD (const char *, int *, int *, int *, int *, int *, int *,
+                      int *, int *, char *);
 
 // This function selects a part of the area.
-void SELECT(int *, int *, int *, int *, int, int, int *,int);
+void SELECT (int *, int *, int *, int *, int, int, int *, int);
 
 // This function selects the whole area.
-void SELECT_ALL(int *, int *, int *, int *, int *, int *,int,int*,int*,int);
+void SELECT_ALL (int *, int *, int *, int *, int *, int *, int, int *, int *,
+                 int);
 
 // This functions swaps the values of two variables
-void swap(int *,int*);
+void swap (int *, int *);
 
 // This function rotates the image or a selection of pixels from the image by a
 // certain angle.
-void ROTATE(const int, unsigned char ***, int *, int *, int *, int *, const int,
-int *, int *,int,int*,int*,int*,int*);
+void ROTATE (const int, unsigned char ***, int *, int *, int *, int *,
+             const int, int *, int *, int, int *, int *, int *, int *);
 
 // This function crops the image.
-void CROP(int *, int *, int *, int *, int *, int *, const int,int*,int*,int*);
+void CROP (int *, int *, int *, int *, int *, int *, const int, int *, int *,
+           int *);
 
 // This function will turn the image to grayscale format.
-void GRAYSCALE(int *, int *, int *, int *, unsigned char ***);
+void GRAYSCALE (int *, int *, int *, int *, unsigned char ***);
 
 // This functions returns the lowest value from the comparison of two values
-int minimum_value(int, int);
+int minimum_value (int, int);
 
 // This function will turn the image to sepia format.
-void SEPIA(int *, int *, int *, int *, unsigned char ***, int *);
+void SEPIA (int *, int *, int *, int *, unsigned char ***, int *);
 
 // This function saves the current image to a file in the specified format.
-void SAVE(const char *, unsigned char **, int, const int, const int,
-const char *, int, int, int, int, int);
+void SAVE (const char *, unsigned char **, int, const int, const int,
+           const char *, int, int, int, int, int);
 
 // This function closes the program and frees the memory.
-void EXIT(unsigned char ***, const int);
+void EXIT (unsigned char ***, const int);
 
 // This functions checks if an valid command has been received
 // and, if true, executes the corresponding function.
-void check_command(const int, int *, int *, int *, int *, int *, int *, int *,
-unsigned char ***, char **, int *, char *, int *, int *, int *,int*,int*,
-int*,int*,int*,int*,int*);
+void check_command (const int, int *, int *, int *, int *, int *, int *, int *,
+                    unsigned char ***, char **, int *, char *, int *, int *,
+                    int *, int *, int *, int *, int *, int *, int *, int *);
 
 // Exit status
-enum {
-	SUCCESS = 0,
-	ERROR = 1,
+enum
+{
+  SUCCESS = 0,
+  ERROR = 1,
 };
 
 #endif
