3b418a6145112f3b8d97b1402da7adc8637382e8
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-01-11 14:05:17 +0200

    Update Memory.h

--- Photo Editor/Memory.h
+++ Photo Editor/Memory.h
@@ -2,7 +2,6 @@
 
 #ifndef __MEMORY_H_
 #define __MEMORY_H_
-#include <stdio.h>
 
 // This function allocates the space necessary for the commands.
 char *user_input(void);
@@ -10,18 +9,19 @@ char *user_input(void);
 // This function returns a value between 0 - 8 representing
 // the position in the command list of the received input
 // and returns -1 if it doesn't exist
-int find_command_value(const char **, const char *);
+int find_command_value(const char *commands[], char *input);
 
 // This functions allocates the memory for the image pixels
 // and copies the pixels from the file in a matrix shape.
-unsigned char **read_pixels(const int, const int, FILE *);
+unsigned char **read_pixels(const int my_image_width, const int my_image_height,
+							FILE *file);
 
 // This functions frees the memory from the currently loaded image.
-void free_pixels(const int, unsigned char ***);
+void free_pixels(const int my_image_height, unsigned char ***pixel_matrix);
 
 // This functions creates a copy of the current selection.
-unsigned char **copy_pixels_selection(
-const int, const int, const int, const int, unsigned char ***);
+unsigned char **copy_pixels_selection(const int x1, const int y1, const int x2,
+									  const int y2, unsigned char ***image);
 
 // Image properties
 typedef struct {
